ea8858a04c6268ace11fac600b3f05aa6edb46a1
François Lachèse <francois.lachese@gmail.com>
2025-03-05 13:11:52 +0100

    feat: add zod for comparison

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,38 @@ import { Hono } from 'hono'
 import { type } from 'arktype'
 import { arktypeValidator } from '@hono/arktype-validator'
 
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
 const schema = type({
   name: 'string',
   age: 'number'
 })
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const schemaZ = z.object({
+  name: z.string(),
+  age: z.number()
 })
 
-app.post('/author', arktypeValidator('json', schema), (c) => {
+const app = new Hono()
+
+
+app.post('/ark', arktypeValidator('json', schema), (c) => {
   const data = c.req.valid('json')
   return c.json({
     success: true,
     message: `${data.name} is ${data.age}`,
   })
 })
+
+app.post(
+  '/zod',
+  zValidator('json', schemaZ), (c) => {
+    const data = c.req.valid('json')
+    return c.json({
+      success: true,
+      message: `${data.name} is ${data.age}`,
+    })
+  })
 
 export default app
